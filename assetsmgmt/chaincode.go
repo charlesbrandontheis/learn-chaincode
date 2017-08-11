ce3137eb6ffaf5aac50e35151a43843178ca3ce3
paperlessnow <sduttajh@in.ibm.com>
2017-08-11 10:51:10 +0530

    trying a simplest code chain file

--- assetsmgmt/chaincode.go
+++ assetsmgmt/chaincode.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+    "fmt"
+
+    "github.com/hyperledger/fabric/core/chaincode/shim"
+    pb "github.com/hyperledger/fabric/protos/peer" 
+)
+
+type SimpleChaincode struct{}
+
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+    return pb.Response{Status: 0, Message: "nothing", Payload: []byte{0, 0}}
+}
+
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+    return pb.Response{Status: 0, Message: "nothing", Payload: []byte{0, 0}}
+}
+
+func main() {
+    err := shim.Start(new(SimpleChaincode))
+    if err != nil {
+        fmt.Printf("Error: %s", err)
+    }
+}
